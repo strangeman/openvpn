13ee82ea6bb1b16bdb92a0bd424aa6d10e2c03e1
Yury Ignatov <yignatov@express42.com>
2014-11-06 10:51:42 +0300

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'info@express42.com'
 license          'MIT'
 description      'Installs/Configures openvpn'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
