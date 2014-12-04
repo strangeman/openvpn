730e14aede1733af7df0b4df599a925df948e4d1
Igor Kurochkin <ikurochkin@express42.com>
2014-12-04 18:53:07 +0300

    Version bump to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,10 @@ name             'openvpn'
 maintainer       'LLC Express 42'
 maintainer_email 'info@express42.com'
 license          'MIT'
-description      'Installs/Configures openvpn'
+description      'Installs and configures OpenVPN'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
+
+recipe           'openvpn::default', 'Installs and configures OpenVPN.'
+
+supports         'ubuntu'
