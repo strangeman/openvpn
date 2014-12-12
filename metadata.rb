fdf637667e0f0057cd5ea7d0905f383d2a3d67e2
Igor Kurochkin <ikurochkin@express42.com>
2014-12-12 16:39:40 +0300

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'openvpn'
 maintainer       'LLC Express 42'
-maintainer_email 'info@express42.com'
+maintainer_email 'cookbooks@express42.com'
 license          'MIT'
-description      'Installs and configures OpenVPN'
+description      'Installs and configures OpenVPN.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.2'
 
